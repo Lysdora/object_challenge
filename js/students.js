b220cc740d504951f2fa7cb284e7c7f77abc3cd6
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-30 11:34:37 +0100

    Create students.js

---
+++ js/students.js
@@ -0,0 +1,37 @@
+
+var students = [
+   {
+     name: 'Sam', 
+     track: 'Web Desing', 
+     achievements: 125, 
+     points: 1250
+   },
+   {
+     name: 'James', 
+     track: 'IOS', 
+     achievements: 750, 
+     points: 5230
+   },
+   {
+     name: 'Helene', 
+     track: 'Web Desing', 
+     achievements: 320, 
+     points: 2205
+   },
+   {
+     name: 'Karl', 
+     track: 'Front End Development', 
+     achievements: 12, 
+     points: 250
+   },
+   {
+     name: 'Lysdora', 
+     track: 'Javascript Development', 
+     achievements: 750, 
+     points: 5028
+   },
+];
+  
+  
+
+
